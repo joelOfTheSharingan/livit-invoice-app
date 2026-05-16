e62f1d6ec8a936ce7542f7f5816cfa5cc5b4a9d1
joelOfTheSharingan <joelmgeorge.2010@icloud.com>
2026-05-16 07:47:16 +0530

    New iport

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { supabase } from "./lib/supabase.js";
-import { useAuth } from "./auth/UseAuth.js";
+import { useAuth } from "./auth/UseAuth";
 import Dashboard from "./Dashboard.jsx";
 import Login from "./Login.jsx";
 
