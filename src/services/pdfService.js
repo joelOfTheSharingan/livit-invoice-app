80acdffb835abf0c358fae50ea381edec795900c
joelOfTheSharingan <joelmgeorge.2010@icloud.com>
2026-05-18 19:21:11 +0530

    New chat

--- src/services/pdfService.js
+++ src/services/pdfService.js
@@ -3,6 +3,6 @@ import { PDF_BASE_URL, IS_LOCAL } from "../utils/constants.js";
 export function getPdfUrl(invoiceId) {
   return IS_LOCAL
     ? `${PDF_BASE_URL}?id=${invoiceId}`
-    : `${PDF_BASE_URL}/${invoiceId}`;
+    : `${PDF_BASE_URL}?id=${invoiceId}`;
     
 }
