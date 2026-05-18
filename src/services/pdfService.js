890d8878f922b545d77f0dec56ed5d499825765a
joelOfTheSharingan <joelmgeorge.2010@icloud.com>
2026-05-18 19:22:13 +0530

    insane

--- src/services/pdfService.js
+++ src/services/pdfService.js
@@ -1,8 +1,6 @@
 import { PDF_BASE_URL, IS_LOCAL } from "../utils/constants.js";
 
 export function getPdfUrl(invoiceId) {
-  return IS_LOCAL
-    ? `${PDF_BASE_URL}?id=${invoiceId}`
-    : `${PDF_BASE_URL}?id=${invoiceId}`;
+  return `${PDF_BASE_URL}?id=${invoiceId}`;
     
 }
